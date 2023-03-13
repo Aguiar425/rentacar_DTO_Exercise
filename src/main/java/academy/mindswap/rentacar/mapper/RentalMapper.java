67dce58ff7cfa84eacd000a0fa6ee8023719e318
Aguiar425 <aguiar425@gmail.com>
2023-03-13 16:37:29 +0000

    please be working lulw

--- src/main/java/academy/mindswap/rentacar/mapper/RentalMapper.java
+++ src/main/java/academy/mindswap/rentacar/mapper/RentalMapper.java
@@ -5,19 +5,18 @@ import academy.mindswap.rentacar.dto.RentalDto;
 import academy.mindswap.rentacar.model.Rental;
 import org.mapstruct.Mapper;
 
-@Mapper (componentModel = "spring")
+@Mapper(componentModel = "spring")
 public interface RentalMapper {
 
-    RentalDto fromRentalEntityToRentalDto(Rental rental);
-   Rental fromRentalCreatedDtoToRentalEntity(RentalCreatedDto rentalCreatedDto);
+    //RentalDto fromRentalEntityToRentalDto(Rental rental);
+    Rental fromRentalCreatedDtoToRentalEntity(RentalCreatedDto rentalCreatedDto);
 
-//    default RentalDto fromRentalEntityToRentalDto(Rental rental){
-//        return RentalDto.builder()
-//                .startDate(rental.getStartDate())
-//                .endDate(rental.getEndDate())
-//                .user(rental.getUser().getFirstName() + " ".concat(rental.getUser().getLastName()))
-//                .car(rental.getCar().getBrand() + " ".concat(rental.getCar().getModel()))
-//                .build();
-//    }
-
+    default RentalDto fromRentalEntityToRentalDto(Rental rental) {
+        return RentalDto.builder()
+                .startDate(rental.getStartDate())
+                .endDate(rental.getEndDate())
+                .user(rental.getUser().getFirstName() + " ".concat(rental.getUser().getLastName()))
+                .car(rental.getCar().getBrand() + " ".concat(rental.getCar().getModel()))
+                .build();
+    }
 }
