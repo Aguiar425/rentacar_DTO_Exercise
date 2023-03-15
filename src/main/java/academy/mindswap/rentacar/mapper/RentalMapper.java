24ca127ade362377c15ea3f8847fe4388d5f1d0f
GutoFaria365 <122914909+GutoFaria365@users.noreply.github.com>
2023-03-15 12:37:47 +0000

    DB migrated to postgres

--- src/main/java/academy/mindswap/rentacar/mapper/RentalMapper.java
+++ src/main/java/academy/mindswap/rentacar/mapper/RentalMapper.java
@@ -3,10 +3,16 @@ package academy.mindswap.rentacar.mapper;
 import academy.mindswap.rentacar.dto.RentalCreatedDto;
 import academy.mindswap.rentacar.dto.RentalDto;
 import academy.mindswap.rentacar.model.Rental;
+import academy.mindswap.rentacar.service.CarServiceImpl;
+import academy.mindswap.rentacar.service.UserServiceImpl;
 import org.mapstruct.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = "spring")
 public interface RentalMapper {
+//    CarServiceImpl carService;
+//    UserServiceImpl userService;
+
 
     //RentalDto fromRentalEntityToRentalDto(Rental rental);
     Rental fromRentalCreatedDtoToRentalEntity(RentalCreatedDto rentalCreatedDto);
@@ -18,5 +24,21 @@ public interface RentalMapper {
                 .user(rental.getUser().getFirstName() + " ".concat(rental.getUser().getLastName()))
                 .car(rental.getCar().getBrand() + " ".concat(rental.getCar().getModel()))
                 .build();
+    }
+    default Rental fromRentalCreatedDtoToRentalEntityTESTE(RentalCreatedDto rentalCreatedDto){
+        if ( rentalCreatedDto == null ) {
+            return null;
+        }
+
+//        carService.getCarById(rentalCreatedDto.getCar().getId());
+//        userService.getUserById(rentalCreatedDto.getUser().getId());
+        Rental.RentalBuilder rental = Rental.builder();
+
+        rental.startDate( rentalCreatedDto.getStartDate() );
+        rental.endDate( rentalCreatedDto.getEndDate() );
+        rental.user( rentalCreatedDto.getUser() );
+        rental.car( rentalCreatedDto.getCar() );
+
+        return rental.build();
     }
 }
