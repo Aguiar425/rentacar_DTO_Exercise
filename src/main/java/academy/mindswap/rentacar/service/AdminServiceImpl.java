a9413cfd81884c978bd312587c1ab620bfb20d51
Aguiar425 <aguiar425@gmail.com>
2023-03-16 12:04:28 +0000

    SERVICE

--- src/main/java/academy/mindswap/rentacar/service/AdminServiceImpl.java
+++ src/main/java/academy/mindswap/rentacar/service/AdminServiceImpl.java
@@ -7,8 +7,10 @@ import academy.mindswap.rentacar.model.User;
 import academy.mindswap.rentacar.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+@Service
 public class AdminServiceImpl implements AdminService{
 
     private UserRepository userRepository;
